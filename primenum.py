4292b85c26d5e62e6b5b14cec1b1850635649def
SowndaryaDhanasekaran <41140237+SowndaryaDhanasekaran@users.noreply.github.com>
2018-07-12 16:53:38 +0530

    Create primenum.py

---
+++ primenum.py
@@ -0,0 +1,12 @@
+number=int(input())
+count=0
+if(number<=1000):
+  for i in range(2,number):
+    if(number%2==0):
+      count=count+1
+  if(count==2):
+    print("yes")
+  else:
+    print("no")
+else:
+  print("no")
