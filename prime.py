bbc89ad43b7bde4d3d2f59f3b775c3d35ced2f9c
SowndaryaDhanasekaran <41140237+SowndaryaDhanasekaran@users.noreply.github.com>
2018-07-12 16:50:23 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,13 +1,12 @@
 number=int(input())
+count=0
 if(number<=1000):
   for i in range(2,number):
     if(number%i==0):
-      flag=1
-    else:
-      flag=0
-  if(flag==1):
-    print("no")
-  else:
+      count=count+1
+  if(count==2):
     print("yes")
+  else:
+    print("no")
 else:
   print("no")
